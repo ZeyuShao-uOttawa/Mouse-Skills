5f9ab23dedd4bfde4c963462ad0001f8f21a420d
Zeyu Shao <zeyu.shao@hotmail.com>
2023-02-21 11:41:19 -0500

    added packages

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 
 import "./assets/main.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 const app = createApp(App);
 
 app.use(createPinia());
 
 app.mount("#app");
+
+
