d149a187265d0dca353c5e232e10ac30836babce
Zeyu Shao <zeyu.shao@hotmail.com>
2023-02-23 01:01:53 -0500

    rough code for home page and nav

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 
 import "./assets/main.css";
+import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(router);
 
 app.mount("#app");
 
---
+++ src/pages/Home.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+    <div class="container" style="text-align: center; margin: 16rem auto; max-width:50%">
+        <div class="row" style="height: 20rem; margin: auto;">
+          <div class="col-6 menu" style="background-color: aquamarine;">Exercises</div>
+          <div class="col-6">
+            <div class="row" style="height: 100%;">
+              <div class="col-12 menu" style="background-color: aqua;">Log In</div>
+              <div class="col-12 menu" style="background-color: blue;">Sign Up</div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+.menu {
+  align-items: center; 
+  justify-content: center; 
+  display: flex;
+  font-size: xx-large;
+}
+</style>
