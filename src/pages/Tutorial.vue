c7c6c0557fc80c189492b04f6742c2771bf8032e
Alielmawazini <126001999+Alielmawazini@users.noreply.github.com>
2023-02-26 21:41:08 -0500

    Creat tutorial

---
+++ src/pages/Tutorial.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<div class="text-center">Tutorial Video</div>
+<div class="row">
+    <div class="col-sm-4 center">
+        <div class="embed-responsive embed-responsive-21by9">
+            <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/DalSs_Go6x4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> 
+        </div>
+    </div>
+</div> 
+</template>
+
+<style scoped>
+.menu {
+  align-items: center; 
+  justify-content: center; 
+  display: flex;
+  font-size: xx-large;
+}
+</style>
