92a26082c7350ce0120148228f673476c958b4d7
Alielmawazini <126001999+Alielmawazini@users.noreply.github.com>
2023-03-06 12:53:31 -0500

    update exercise 1

---
+++ src/pages/Exercise1.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+<div class="container">
+    <div class="text-center"><h2>Clicker Game</h2></div>
+    <div class="text-top-right"><h3>Drag the mouse and right click on the image. This is to practise right clicking</h3></div>
+    <div class="row">
+        <div class="col-12 center" >
+            <div><img src="C:\Users\alima\Documents\GitHub\Mouse-Skills\src\assets\Red_Circle(small).svg.png" class="img-fluid" alt="Responsive image"></div>
+  
+            
+        </div>
+    </div> 
+</div>
+</template>
+
+<style scoped>
+
+</style>
