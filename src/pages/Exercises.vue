ffa3c420c3809316bfcbb7a3a1514639f0ee6a56
Zeyu Shao <zeyu.shao@hotmail.com>
2023-03-10 02:17:57 -0500

    Added exercises page

---
+++ src/pages/Exercises.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+    <div class="container" style="text-align: center; margin: auto; height: 92vh;">
+        <div class="text-center"><h2>Exercises</h2></div>
+        <div class="card-group text-center center" style="margin: auto;">
+            <div class="col-lg-3" id="project-card-div">
+            <a class="card-link" href="#">
+                <div class="card" id="project-card">
+                <img src="Assets/study1.png" class="card-img-top" alt="NOT LOADING" id="project-img">
+                <div class="card-body">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                </div>
+            </a>
+            </div>
+            <div class="col-lg-1">
+
+            </div>
+            <div class="col-lg-3" id="project-card-div">
+            <a class="card-link" href="#">
+                <div class="card" id="project-card">
+                <img src="Assets/study1.png" class="card-img-top" alt="NOT LOADING" id="project-img">
+                <div class="card-body">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                </div>
+            </a>
+            </div>
+            <div class="col-lg-1">
+
+            </div>
+            <div class="col-lg-3" id="project-card-div">
+            <a class="card-link" href="#">
+                <div class="card" id="project-card">
+                <img src="Assets/study1.png" class="card-img-top" alt="NOT LOADING" id="project-img">
+                <div class="card-body">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                </div>
+            </a>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+.center {
+  align-items: center; 
+  justify-content: center; 
+  display: flex;
+}
+
+</style>
