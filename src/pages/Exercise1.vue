a5cbb347bd49c72f867c73775c41db4ea85ddcec
Alielmawazini <126001999+Alielmawazini@users.noreply.github.com>
2023-03-11 13:07:03 -0500

    Exercise1update

--- src/pages/Exercise1.vue
+++ src/pages/Exercise1.vue
@@ -1,21 +1,59 @@
-<script setup lang="ts">
-</script>
-
 <template>
-
-<div class="container">
-    <div class="text-center"><h2>Clicker Game</h2></div>
-    <div class="text-top-right"><h3>Drag the mouse and right click on the image. This is to practise right clicking</h3></div>
-    <div class="row">
-        <div class="col-12 center" >
-            <div><img src="C:\Users\alima\Documents\GitHub\Mouse-Skills\src\assets\Red_Circle(small).svg.png" class="img-fluid" alt="Responsive image"></div>
+    <div>
+      <h1>Click the button 5 times!</h1>
+      <div class="game-area" @click="handleClick">
+        <button v-if="showButton" class="game-button"></button>
+      </div>
+      <p v-if="gameCompleted">Game completed!</p>
+    </div>
+  </template>
   
-            
-        </div>
-    </div> 
-</div>
-</template>
-
-<style scoped>
-
-</style>
+  <script>
+  export default {
+    data() {
+      return {
+        showButton: false,
+        score: 0,
+        gameCompleted: false,
+        maxPresses: 5,
+      };
+    },
+    methods: {
+      handleClick() {
+        if (this.showButton) {
+          this.score++;
+          if (this.score >= this.maxPresses) {
+            this.gameCompleted = true;
+          }
+          this.showButton = false;
+          setTimeout(() => {
+            this.showButton = true;
+          }, Math.random() * 2000 + 1000);
+        }
+      },
+    },
+    mounted() {
+      this.showButton = true;
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .game-area {
+    width: 400px;
+    height: 400px;
+    border: 2px solid black;
+    position: relative;
+  }
+  .game-button {
+    position: absolute;
+    left { Math.random() * 350 }px;
+    top { Math.random () * 350 }px;
+    width: 50px;
+    height: 50px;
+    background-color: red;
+    border: none;
+    border-radius: 50%;
+  }
+  </style>
+  
