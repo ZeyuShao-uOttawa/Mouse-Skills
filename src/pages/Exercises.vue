0362204d3056c10bc9a26b169ecf0e3c55c72c88
Zeyu Shao <zeyu.shao@hotmail.com>
2023-03-20 12:14:44 -0400

    fixed git merge mistakes

--- src/pages/Exercises.vue
+++ src/pages/Exercises.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
-
-    <div class="container" style="text-align: center; margin: auto; height: 92vh;">
+@@ -8,43 +7,43 @@ import { RouterLink } from "vue-router";
         <div class="text-center"><h2>Exercises</h2></div>
         <div class="card-group text-center center" style="margin: auto;">
             <div class="col-lg-3" id="project-card-div">
-            <a class="card-link" href="#">
+            <a class="card-link" href="#"><RouterLink class="navbar-brand" to="/exercise1">
                 <div class="card" id="project-card">
                 <img src="Assets/study1.png" class="card-img-top" alt="NOT LOADING" id="project-img">
                 <div class="card-body">
@@ -15,13 +15,13 @@
                     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 </div>
                 </div>
-            </a>
+            </RouterLink></a>
             </div>
             <div class="col-lg-1">
 
             </div>
             <div class="col-lg-3" id="project-card-div">
-            <a class="card-link" href="#">
+            <a class="card-link" href="#"><RouterLink class="navbar-brand" to="/exercise2">
                 <div class="card" id="project-card">
                 <img src="Assets/study1.png" class="card-img-top" alt="NOT LOADING" id="project-img">
                 <div class="card-body">
@@ -29,13 +29,13 @@
                     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 </div>
                 </div>
-            </a>
+            </RouterLink></a>
             </div>
             <div class="col-lg-1">
 
             </div>
             <div class="col-lg-3" id="project-card-div">
-            <a class="card-link" href="#">
+            <a class="card-link" href="#"><RouterLink class="navbar-brand" to="/exercise3">
                 <div class="card" id="project-card">
                 <img src="Assets/study1.png" class="card-img-top" alt="NOT LOADING" id="project-img">
                 <div class="card-body">
@@ -43,18 +43,7 @@
                     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 </div>
                 </div>
-            </a>
+            </RouterLink></a>
             </div>
         </div>
     </div>
-
-</template>
-
-<style scoped>
-.center {
-  align-items: center; 
-  justify-content: center; 
-  display: flex;
-}
-
-</style>
