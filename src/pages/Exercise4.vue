6315e220a1a907c2ff2c46c686b5df87b11dc306
Alielmawazini <126001999+Alielmawazini@users.noreply.github.com>
2023-03-26 12:14:35 -0400

    AlicommintPart7

--- src/pages/Exercise4.vue
+++ src/pages/Exercise4.vue
@@ -1,75 +1,139 @@
 <template>
-  <div class="container">
-    <div class="box" :style="{ top: boxTop + 'px', left: boxLeft + 'px' }" ref="box" @mouseenter="respawnBox">
-      <p v-if="showMessage" class="message">Good job, you have succeeded!</p>
+    <div class="container">
+      <div
+        class="box"
+        :style="{ top: boxTop + 'px', left: boxLeft + 'px' }"
+        ref="box"
+        @mouseenter="respawnBox"
+      >
+        <p v-if="showMessage" class="message">Good job, you have succeeded!</p>
+      </div>
+      <div
+        class="item"
+        v-for="(item, index) in items"
+        :key="index"
+        :style="{ top: item.top + 'px', left: item.left + 'px' }"
+        ref="item"
+        @mousedown="startDrag(index)"
+        @mouseup="stopDrag(index)"
+      ></div>
+      <button class="hint-btn" @click="showHint">Hint</button>
+      <div class="hint-modal" v-if="showHintModal">
+        <video class="hint-video" controls>
+          <source src="./hint.mp4" type="video/mp4" />
+        </video>
+        <button class="close-btn" @click="closeHint">Close</button>
+      </div>
     </div>
-    <div class="item" v-for="(item, index) in items" :key="index" :style="{ top: item.top + 'px', left: item.left + 'px' }" ref="item" @mousedown="startDrag(index)" @mouseup="stopDrag(index)"></div>
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      dragging: false,
-      mouseX: 0,
-      mouseY: 0,
-      boxTop: 100,
-      boxLeft: 400,
-      showMessage: false,
-      items: [
-        { top: 200, left: 200 },
-        { top: 300, left: 300 },
-        { top: 400, left: 400 }
-      ]
-    };
-  },
-  methods: {
-    startDrag(index) {
-      this.dragging = index;
-      this.mouseX = event.clientX;
-      this.mouseY = event.clientY;
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        dragging: false,
+        mouseX: 0,
+        mouseY: 0,
+        boxTop: 100,
+        boxLeft: 400,
+        showMessage: false,
+        items: [
+          { top: 200, left: 200 },
+          { top: 300, left: 300 },
+          { top: 400, left: 400 },
+        ],
+        showHintModal: false,
+      };
     },
-    stopDrag(index) {
-      this.dragging = false;
-      const item = this.items[index];
-      if (item.left > this.boxLeft && item.left < this.boxLeft + 100 && item.top > this.boxTop && item.top < this.boxTop + 100) {
-        this.items.splice(index, 1);
-        this.showMessage = true;
-      }
-    },
-    handleDrag(event) {
-      if (this.dragging !== false) {
-        const item = this.items[this.dragging];
-        item.left += event.clientX - this.mouseX;
-        item.top += event.clientY - this.mouseY;
+    methods: {
+      startDrag(index) {
+        this.dragging = index;
         this.mouseX = event.clientX;
         this.mouseY = event.clientY;
-      }
+      },
+      stopDrag(index) {
+        this.dragging = false;
+        const item = this.items[index];
+        if (
+          item.left > this.boxLeft &&
+          item.left < this.boxLeft + 100 &&
+          item.top > this.boxTop &&
+          item.top < this.boxTop + 100
+        ) {
+          this.items.splice(index, 1);
+          if (this.items.length === 0) {
+            this.respawnItems();
+          }
+          this.showMessage = true;
+        }
+      },
+      handleDrag(event) {
+        if (this.dragging !== false) {
+          const item = this.items[this.dragging];
+          item.left += event.clientX - this.mouseX;
+          item.top += event.clientY - this.mouseY;
+          this.mouseX = event.clientX;
+          this.mouseY = event.clientY;
+        }
+      },
+      respawnBox() {
+        this.boxTop = Math.random() * (window.innerHeight - 100);
+        this.boxLeft = Math.random() * (window.innerWidth - 100);
+        this.showMessage = false;
+      },
+      respawnItems() {
+        this.items = [
+          { top: 200, left: 200 },
+          { top: 300, left: 300 },
+          { top: 400, left: 400 },
+        ];
+      },
+      showHint() {
+        this.showHintModal = true;
+      },
+      closeHint() {
+        this.showHintModal = false;
+      },
     },
-    respawnBox() {
-      this.boxTop = Math.random() * (window.innerHeight - 100);
-      this.boxLeft = Math.random() * (window.innerWidth - 100);
-      this.showMessage = false;
+    mounted() {
+      window.addEventListener("mousemove", this.handleDrag);
+    },
+    beforeUnmount() {
+      window.removeEventListener("mousemove", this.handleDrag);
+    },
+  };
+  mounted() {
+  window.addEventListener("mousemove", this.handleDrag);
+  window.addEventListener("keydown", this.handleKeyDown);
+},
+beforeUnmount() {
+  window.removeEventListener("mousemove", this.handleDrag);
+  window.removeEventListener("keydown", this.handleKeyDown);
+},
+methods: {
+  handleKeyDown(event) {
+    if (event.key === "a") {
+      this.showHintModal = true;
     }
   },
-  mounted() {
-    window.addEventListener("mousemove", this.handleDrag);
-  },
-  beforeUnmount() {
-    window.removeEventListener("mousemove", this.handleDrag);
-  }
-};
-</script>
-
-<style scoped>
-.container {
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 }
 
+
+
+  </script>
+  
+  <style scoped>
+  .hint-btn {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+}
+  .container {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 .box {
   position: absolute;
   width: 100px;
@@ -99,3 +163,4 @@ export default {
   cursor: grab;
 }
 </style>
+  
